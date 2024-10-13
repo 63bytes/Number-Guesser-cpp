48f22d589dce8d13411c0c8b120daf1e956f803c
63bytes <>
2024-10-13 17:52:25 +0100

    finish

--- Number Guesser cpp/Number Guesser cpp.cpp
+++ Number Guesser cpp/Number Guesser cpp.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <random>
+#include <string>
+using namespace std;
 
 int randint(int min, int max)
 {
@@ -14,10 +17,44 @@ int randint(int min, int max)
 
 void runGame(int number)
 {
-  //  while (true)
+    int guess;
+    while (guess!=number)
+    {
+        cout << "Guess a number:";
+        cin >> guess;
+        if (guess<number)
+        {
+            cout << "Guess higher" << endl;
+        }
+        if (guess>number)
+        {
+            cout << "Guess lower" << endl;
+        }
+    }
+    cout << "You guessed correct, the number was " + to_string(number) << endl;
 }
 
 int main()
 {
-    std::cout << "Hello World!" std::endl;
+    int min,max;
+    string answer;
+    cout << "Minimum number:";
+    cin >> min;
+    cout << "Maximum number:";
+    cin >> max;
+    runGame(randint(min,max));
+    while (true)
+    {
+        cout << "Enter 'y' to play again?";
+        cin >> answer;
+        if (answer=="y")
+        {
+            runGame(randint(min,max));
+        }
+        else
+        {
+            break;
+        }
+    }
+    return 0;
 }
